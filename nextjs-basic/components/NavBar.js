1700967576113d2eb88d0be9dd27358ad6b5833e
2theMoon <dlaguddh1@gmail.com>
2022-02-17 12:58:37 +0900

    한 태그에 클래스이름 두개 넣기
    - 하나는 정적
    - 하나는 동적
    문자리터럴 방식,
    배열 조인 방식

--- nextjs-basic/components/NavBar.js
+++ nextjs-basic/components/NavBar.js
@@ -8,10 +8,13 @@ export default function NavBar() {
     return (
         <nav>
             <Link href="/">
-                <a className={router.pathname === "/" ? styles.active : ""}>Home</a>
+                <a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""
+                    }`}>Home</a>
             </Link>
             <Link href="/about">
-                <a className={router.pathname === "/about" ? styles.active : ""}>About</a>
+                <a className={[
+                    styles.link,
+                    router.pathname === "/about" ? styles.active : ""].join(" ")}>About</a>
             </Link >
         </nav >
     );
